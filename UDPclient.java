0baefe6197c4fdb915bbf1b08ed1efb2bc694f14
windoge <jmbaker9230@gmail.com>
2015-02-14 23:27:22 -0600

    again

--- UDPclient.java
+++ UDPclient.java
@@ -8,7 +8,7 @@ class UDPClient
       BufferedReader inFromUser =
          new BufferedReader(new InputStreamReader(System.in));
       DatagramSocket clientSocket = new DatagramSocket();
-      InetAddress IPAddress = InetAddress.getByName("localhost");
+      InetAddress IPAddress = InetAddress.getByName("192.168.1.99");
       byte[] sendData = new byte[1024];
       byte[] receiveData = new byte[1024];
       String sentence = inFromUser.readLine();
