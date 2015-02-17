1ae0e01b268452b488f44826dc2fd528e74703b3
thatbakerguy <jmbaker9230@gmail.com>
2015-02-17 16:06:36 -0500

    localhost

--- TCP/TCPclient.java
+++ TCP/TCPclient.java
@@ -8,7 +8,7 @@ class TCPclient {
   BufferedReader inFromUser =
    new BufferedReader(
      new InputStreamReader(System.in));
-  Socket clientSocket = new Socket("hostname", 6789);
+  Socket clientSocket = new Socket("localhost", 6789);
   DataOutputStream outToServer =
    new DataOutputStream(
      clientSocket.getOutputStream());
