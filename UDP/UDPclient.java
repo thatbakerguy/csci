edac9a365059c315d073cbac466e67097870eabd
thatbakerguy <jmbaker9230@gmail.com>
2015-02-19 23:22:56 -0600

    udp

--- UDP/UDPclient.java
+++ UDP/UDPclient.java
@@ -8,7 +8,7 @@ class UDPClient
       BufferedReader inFromUser =
          new BufferedReader(new InputStreamReader(System.in));
       DatagramSocket clientSocket = new DatagramSocket();
-      InetAddress IPAddress = InetAddress.getByName("192.168.1.99");
+      InetAddress IPAddress = InetAddress.getByName("192.168.1.99");//same as tcp  set this as the local ip  of server
       byte[] sendData = new byte[1024];
       byte[] receiveData = new byte[1024];
       String sentence = inFromUser.readLine();
